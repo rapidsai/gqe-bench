rapidsai/gqe-bench

commit 95a5c02650bc10013eea21cd496185c4ad569871

Allow specifying the quries to run. (haog/gqe-python!50)
Either repeat the  option or supply multiple queries, space-separated, to the  option.

gqe/benchmark/run_tpch.py
@@ -41,6 +41,7 @@ def main():
     arg_parser.add_argument("location", help="TPC-H dataset location")
     arg_parser.add_argument("solution", help="Reference results location with pattern")
     arg_parser.add_argument("--output", "-o", help="Output file path")
+    arg_parser.add_argument("--queries", "-q", help="Which queries to run", nargs="+", action="extend")
     args = arg_parser.parse_args()
 
     num_row_groups = 8
@@ -71,7 +72,8 @@ def main():
             catalog = Catalog()
             catalog.register_tpch(args.location, storage, num_row_groups, 0,  identifier_type, use_opt_char_type)
 
-        for query_idx in [1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 15, 17, 18, 19, 20, 21]:
+        queries = args.queries if args.queries else [1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 15, 17, 18, 19, 20, 21]
+        for query_idx in queries:
             if not load_all_data and (storage == "memory"):
                 catalog = Catalog()
                 catalog.register_tpch(args.location, storage,  num_row_groups, query_idx, identifier_type, use_opt_char_type)

commit c3b099eb808b9bfc533fd2b8103015b471c1adb8

Queries arg should be numeric. (haog/gqe-python!66)
This fixes a bug introduced in 95a5c02. Brings https://gitlab-master.nvidia.com/haog/gqe-python/-/merge_requests/52 up to date

gqe/benchmark/run_tpch.py
@@ -41,7 +41,8 @@ def main():
     arg_parser.add_argument("location", help="TPC-H dataset location")
     arg_parser.add_argument("solution", help="Reference results location with pattern")
     arg_parser.add_argument("--output", "-o", help="Output file path")
-    arg_parser.add_argument("--queries", "-q", help="Which queries to run", nargs="+", action="extend")
+    arg_parser.add_argument("--queries", "-q", help="Which queries to run", nargs="+",
+                            action="extend", type=int)
     args = arg_parser.parse_args()
 
     num_row_groups = 8